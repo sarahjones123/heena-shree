a0a2fe1e0024bf31dd7ebce0896cdde4b20f751f
heena_shree <heena1shree@gmail.com>
2026-02-15 01:37:20 +0530

    Deleted the unwanted files

--- docs/assets/js/removeCredits.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    removeCredits();
-});
-
-function removeCredits(){
-//    document.querySelectorAll('.md-copyright')[0].getElementsByTagName("a")[0].remove();
-//    document.querySelectorAll('.md-copyright')[0].childNodes[1].remove();
-}
